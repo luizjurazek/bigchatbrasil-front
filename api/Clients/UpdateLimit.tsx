fb8df7026e04b54b743316953c2fc9f5ade004c5
Luiz Jurazek <luizjurazek@gmail.com>
2025-02-23 15:24:21 -0300

    feat: create the function to update the limit

---
+++ api/Clients/UpdateLimit.tsx
@@ -0,0 +1,17 @@
+import { Client } from "@/types/ClientTypes";
+import axios from "axios";
+
+export const UpdateLimit = async (id: number, newLimit: number): Promise<Client> => {
+  const api = process.env.NEXT_PUBLIC_API_URL;
+  const data = {
+    newLimit: newLimit
+  }
+  try {
+    const response = await axios.put(`${api}/clients/update-limit/${id}`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao atualizar o limite:", error);
+    throw error;
+  }
+}
+
